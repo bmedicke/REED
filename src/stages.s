48946cf0de10ed09e20f2f7f5bbe49fd6342ead3
Benjamin Medicke <bmedicke@gmail.com>
2020-10-04 09:31:48 +0200

    add binary and intermediary files

---
+++ src/stages.s
@@ -0,0 +1,69 @@
+	.file	"stages.c"
+	.intel_syntax noprefix
+	.text
+	.section	.rodata
+.LC0:
+	.string	"hello, world!"
+	.text
+	.globl	greet
+	.type	greet, @function
+greet:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	lea	rdi, .LC0[rip]
+	call	puts@PLT
+	nop
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	greet, .-greet
+	.globl	main
+	.type	main, @function
+main:
+.LFB7:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	sub	rsp, 32
+	mov	DWORD PTR -4[rbp], edi
+	mov	QWORD PTR -16[rbp], rsi
+	mov	QWORD PTR -24[rbp], rdx
+	mov	eax, 0
+	call	greet
+	mov	eax, 0
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE7:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
